0d8668917b7dd4ecd3184e0865c71cb9a0195546
Kim-Onesmus <kimones1122@gmail.com>
2026-02-22 15:15:04 +0300

    reset git ignore

--- rawunwind/settings.py
+++ rawunwind/settings.py
@@ -27,7 +27,6 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    # 'storages',
     'ckeditor',
     'ckeditor_uploader',
     'app',
@@ -211,24 +210,6 @@ CKEDITOR_CONFIGS = {
         'pasteFromWordRemoveFontStyles': True,
     }
 }
-
-
-# else:
-#     STORAGES = {
-#         "default": {
-#             "BACKEND": "storages.backends.s3boto3.S3Boto3Storage",
-#         },
-#         "staticfiles": {
-#             "BACKEND": "django.contrib.staticfiles.storage.StaticFilesStorage",
-#         },
-#     }
-#     AWS_ACCESS_KEY_ID = os.getenv('AWS_ACCESS_KEY_ID')
-#     AWS_SECRET_ACCESS_KEY = os.getenv('AWS_SECRET_ACCESS_KEY')
-#     AWS_STORAGE_BUCKET_NAME = os.getenv('AWS_STORAGE_BUCKET_NAME')
-#     AWS_S3_REGION_NAME = os.getenv('AWS_S3_REGION_NAME')
-#     AWS_QUERYSTRING_EXPIRE = 6000
-
-#     CKEDITOR_STORAGE_BACKEND = 'storages.backends.s3boto3.S3Boto3Storage'
 
 
 
