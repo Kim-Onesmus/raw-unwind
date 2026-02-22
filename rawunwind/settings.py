55f22a00c827eb622500a5f648feb8f5077af75b
Kim-Onesmus <kimones1122@gmail.com>
2026-02-22 14:54:29 +0300

    database reset

--- rawunwind/settings.py
+++ rawunwind/settings.py
@@ -27,7 +27,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'storages',
+    # 'storages',
     'ckeditor',
     'ckeditor_uploader',
     'app',
